659fe1917a3a2b225937572e7eb652044724dc4c
SELVA7092 <selvarockz02@gmail.com>
2024-09-26 15:44:08 +0530

    update

--- src/App.js
+++ src/App.js
@@ -5,18 +5,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./component/navbar.jsx";
 import Signlog from './component/signlog.jsx'
 import Createacc from './component/createacc.jsx'
-
 import Home from './component/home.jsx'
+import Products from './component/products.jsx';
 
 function App() {
 
     return (
 
-        <BrowserRouter>
-            <Routes> 
-                <Route path="/" element={<Home />} />
-                <Route path="/signlog" element={<Signlog />} />
-                <Route path="/createacc" element={<Createacc />} />
+        <BrowserRouter >
+            <Routes >
+                <Route path="/" element={< Home />} />
+                <Route path="/signlog" element={< Signlog />} />
+                <Route path="/createacc" element={< Createacc />} />
+                <Route path="/product" element={<Products />} />
             </Routes>
         </BrowserRouter>
 
---
+++ src/component/products.jsx
@@ -0,0 +1,26 @@
+import Navbars from "./navbar.jsx";
+import swiperpro from "../assets/swiperpro.jsx";
+import "products.scss"
+
+function Products(){
+return(
+<>
+<Navbars />
+<hr />
+{/* <div className="productview">
+<div className="productleft">
+<img src="${swiperpro[0].img}" />
+</div>
+
+
+<div className="productright">
+
+</div>
+</div> */}
+
+
+</>
+);
+
+}
+export default Products;
